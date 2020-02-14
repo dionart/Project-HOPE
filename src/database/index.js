5af0ec142d15075a93d1c0c51b5545dfce9f82d5
Arthur Dionízio <47128865+dionart@users.noreply.github.com>
2020-02-14 03:17:54 -0300

    Add files via upload

--- src/database/index.js
+++ src/database/index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://dionart:camisetas123@cluster0-veaas.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true, 
+    useFindAndModify: false,
 });
 
 mongoose.Promise = global.Promise;
